243aae60fad65fe2d2d682cb1289a2bacd79738a
Anagha <aka.anaghasujith@gmail.com>
2022-11-26 07:36:31 +0000

    Update 5 files

--- components/about.js
+++ components/about.js
@@ -6,6 +6,7 @@ function About() {
         'Eleventy',
         'Node.js',
         'WordPress',
+        'goland',
     ];
     return (
         <div
@@ -14,6 +15,42 @@ function About() {
             <h2 className='about-heading flex items-center w-full mb-20 whitespace-nowrap leading-[1.1] font-semibold text-slate-300 text-step-5'>
                 About Me
             </h2>
+            <div className="Content_container flex gap-12 flex-col sm:flex-row">
+                <div className="content_left">
+                    <div className="content_aboutme"></div>
+                     <p className=""> 
+                         vcjsdv jdbcdcdgcdy cygcd ugygc ydcgydg cygc dygcygc dycyc xdgc ygdyg dy sdy
+                         chd cdvhcuidc sdjcidjd
+                         nc djusdhvduhvudhsdo hsduhc udhudx
+                         
+                     </p>
+                     <p className="mt-4">
+                         vcjsdv jdbcdcdgcdy cygcd ugygc ydcgydg cygc{" "}
+                          <span className="text-emerald-300">dygcygc</span> dycyc xdgc ygdyg dy sdy
+                         chd cdvhcuidc sdjcidjd
+                         nc djusdhvduhvudhsdo hsduhc udhudx  
+                     </p>
+                     <p className="font-bold mt-8"> 
+                            Here are some technologies i'm familiar with:{" "}
+                            </p>
+                    <div className="content_technologies text-slate-300 grid grid-cols-2 mt-8">
+                            
+                        {
+                            skills.map(
+                                (skill)=>{
+                                    return <p key={skill}>{skill}</p>
+                                }
+                            )
+                        }
+                
+                    </div>
+                        
+                </div>
+                <div className="Content_right">
+                    <img src="/rick.gif" className="w-full sm:w-3/4"/>
+                    
+                </div>
+            </div>
         </div>
     );
 }
--- components/projects.js
+++ components/projects.js
@@ -1,4 +1,12 @@
+import ProjectCard from './project_card';
+import {project} from "../data/projects";
 function Projects(props) {
+    const project ={
+        is_featured: false,
+        title: "potfolio project using react"
+        desc: 'et vitae inventjc c duc huic c ucu cc igig si'
+        tags:["react","python"]
+    }
     return (
         <div
             className='flex flex-col gap-4 overflow-scroll max-w-7xl mx-auto px-8 py-20'
@@ -6,6 +14,18 @@ function Projects(props) {
             <h2 className='mb-20 whitespace-nowrap leading-[1.1] font-semibold text-slate-300 text-step-5'>
                 Projects
             </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {project.map(
+                    (project)=> {
+                        return <ProjectCard key={project.title} project={project}/>;
+                    }
+
+                )
+                <ProjectCard project={project}/>
+                <ProjectCard project={project}/> 
+                <ProjectCard project={project}/> 
+            </div>
+            
         </div>
     );
 }
